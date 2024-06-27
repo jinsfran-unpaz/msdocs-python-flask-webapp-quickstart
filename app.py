11e0e505dca100530e2f7a4dcd22e532727284be
Jorge Insfran <jorge.insfran@docentes.unpaz.edu.ar>
2024-06-27 11:32:09 -0300

    chore: Update Flask web server configuration and dependencies

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+""" Main application file for the Flask web server. """
 import os
 
 from flask import (Flask, redirect, render_template, request,
@@ -11,11 +12,19 @@ from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from dotenv import load_dotenv
 
 load_dotenv()
+os.environ["OPENAI_API_VERSION"]="2023-07-01-preview"
+os.environ["OPENAI_CHAT_MODEL"]="gpt-35-turbo"
+os.environ["SQL_SERVER"]="joi.database.windows.net;PORT=1433"
+os.environ["SQL_DB"]="northwind"
 
-connection_string = os.environ["AZURE_SQL_CONNECTIONSTRING"]
-
+DRIVER = '{ODBC Driver 17 for SQL Server}'
 odbc_str = 'mssql+pyodbc:///?odbc_connect=' \
-                + connection_string
+                'Driver='+DRIVER+ \
+                ';Server=tcp:' + os.getenv("SQL_SERVER")+ \
+                ';DATABASE=' + os.getenv("SQL_DB") + \
+                ';Uid=' + os.getenv("SQL_USERNAME")+ \
+                ';Pwd=' + os.getenv("SQL_PWD") + \
+                ';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 
 db_engine = create_engine(odbc_str)
 
@@ -25,19 +34,22 @@ llm = AzureChatOpenAI(
     azure_endpoint=os.getenv("AZURE_ENDPOINT"),
     api_version=os.getenv("OPENAI_API_VERSION")
 )
-
+# pylint: disable=line-too-long
 final_prompt = ChatPromptTemplate.from_messages(
     [
         ("system",
          """
          You are an agent designed to interact with a SQL database.
-         Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer. 
-         Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 10 results.
+         Given an input question, create a syntactically correct {dialect} query to run, 
+         then look at the results of the query and return the answer. 
+         Unless the user specifies a specific number of examples they wish to obtain, 
+         always limit your query to at most 10 results.
          You can order the results by a relevant column to return the most interesting examples in the database.
          Never query for all the columns from a specific table, only ask for the relevant columns given the question.
          You have access to tools for interacting with the database.
          Only use the given tools. Only use the information returned by the tools to construct your final answer.
-         You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+         You MUST double check your query before executing it. If you get an error while executing a query, 
+         rewrite the query and try again.
          
          DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
          If the question does not seem related to the database, just return "No lo sé, pero estoy aprendiendo" as the answer.
@@ -48,6 +60,7 @@ final_prompt = ChatPromptTemplate.from_messages(
         ("user", "{question}\n ai: "),
     ]
 )
+# pylint: enable=line-too-long
 
 db = SQLDatabase(db_engine)
 
@@ -67,31 +80,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    """ Main route for the web server. """    
+    print('Request for index page received')
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
+    """ Route for the favicon. """
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
-   resultado = sqldb_agent.invoke(final_prompt.format(
-       question=name,
-       dialect="SQL Server"
-   ))
-   # resultado es un json que tiene input y output. output es el resultado de la query
-   resultado = resultado['output']
+    """ Route for the hello page."""    
+    name = request.form.get('name')
+    resultado = sqldb_agent.invoke(final_prompt.format(
+        question=name,
+        dialect="SQL Server"
+        ))
+    # resultado es un json que tiene input y output. output es el resultado de la query
+    resultado = resultado['output']
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = resultado)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    if name:
+        print('Request for hello page received with name=%s', name)
+        return render_template('hello.html', name = resultado)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
